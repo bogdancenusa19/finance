85e80e7b86f9fa211b870137a963568c32de3afb
bogdancenusa19 <144590943+bogdancenusa19@users.noreply.github.com>
2026-02-28 22:18:53 +0200

    Update Supabase client configuration
    Replaced environment variables for Supabase URL and key with hardcoded values.

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://cmvqmtfy1ugnhcnsuojn.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtdnFtdGZ5aXVnbmhjbnN1b2puIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyODI5MzQsImV4cCI6MjA4Nzg1ODkzNH0.pW8v8gB2L_lU9s-Kqdx2zFGF6w9MS6pk6OyG3Zhs9UE'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
