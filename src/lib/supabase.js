b00fd288ea54457448b1b72c35340c19ac248e54
bogdancenusa19 <144590943+bogdancenusa19@users.noreply.github.com>
2026-03-01 18:19:49 +0200

    Refactor supabase client initialization

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://cmvqmtfy1ugnhcnsuojn.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtdnFtdGZ5aXVnbmhjbnN1b2puIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyODI5MzQsImV4cCI6MjA4Nzg1ODkzNH0.pW8v8gB2L_lU9s-Kqdx2zFGF6w9MS6pk6OyG3Zhs9UE'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+  'https://cmvqmtfy1ugnhcnsuojn.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtdnFtdGZ5MXVnbmhjbnN1b2puIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5OTU2NzUsImV4cCI6MjA1ODU3MTY3NX0.pw8v8gB2l_1U9s-Kqdx2zFGF6w9MS6pk6OyG3Zhs9UE'
+)
